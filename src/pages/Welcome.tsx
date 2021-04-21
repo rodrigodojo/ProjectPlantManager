60dd5c272f790d6c81f832c8b6a176508487bfa6
Rodrigo Jorge <rodrigodoj@gmail.com>
2021-04-21 17:56:39 -0300

    modificando a primeira activity

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Text, Image ,TouchableOpacity,StyleSheet } from 'react-native';
+import {Text, Image ,TouchableOpacity,StyleSheet,Dimensions } from 'react-native';
 import {SafeAreaView } from 'react-native-safe-area-context';
+import {Entypo} from '@expo/vector-icons';
 
 import wateringImg from '../assets/watering.png';  
 import colors from '../colors';
@@ -11,15 +12,19 @@ export function Welcome(){
         <Text style={styles.title}>
             Gerencie{'\n'}suas plantas{'\n'}de forma fácil
         </Text>
-        <Image source={wateringImg} style={styles.image}/>
+        <Image 
+        source={wateringImg} 
+        style={styles.image}
+        resizeMode="contain"
+        />
         <Text style={styles.subtitle}>
             Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.
         </Text>
-        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-            <Text style={styles.buttonText}>
-                >
-            </Text>
-        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} activeOpacity={0.5}>
+        <Text style={styles.buttonText}>
+            <Entypo name="chevron-thin.right" />
+        </Text>
+    </TouchableOpacity>
     </SafeAreaView>
   )
 }
@@ -28,7 +33,7 @@ const styles =StyleSheet.create({
     container: {
         flex: 1,
         alignItems:'center',
-        justifyContent: 'space-between'        
+        justifyContent: 'space-around'        
     },
     title: {
         fontSize: 32,
@@ -42,6 +47,9 @@ const styles =StyleSheet.create({
         paddingHorizontal:20,
         color: colors.heading
     },
+    image: {
+        height: Dimensions.get('window').width * 0.7
+    },
     button: {
         backgroundColor: colors.green,
         justifyContent: 'center',
@@ -50,10 +58,6 @@ const styles =StyleSheet.create({
         marginBottom:10,
         height:56,
         width:56
-    },
-    image: {
-        width:292,
-        height:284
     },
     buttonText: {
         fontSize: 24,
