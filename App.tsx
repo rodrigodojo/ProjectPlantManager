af8d17acd40d17d3de16b9802894f1faa2306019
Rodrigo Jorge <rodrigodoj@gmail.com>
2021-04-21 17:33:44 -0300

    criando a primeira activity

--- App.tsx
+++ App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Welcome } from './src/pages/Welcome';
 
 export default function App(){
   return(
-    <View style={{flex: 1,justifyContent: 'center',alignItems: 'center'}}>
-    <Text>
-      NLW#5
-    </Text>
-    </View>
+    <Welcome />
   )
 }
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {Text, Image ,TouchableOpacity,StyleSheet } from 'react-native';
+import {SafeAreaView } from 'react-native-safe-area-context';
+
+import wateringImg from '../assets/watering.png';  
+import colors from '../colors';
+
+export function Welcome(){
+  return(
+    <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>
+            Gerencie{'\n'}suas plantas{'\n'}de forma fácil
+        </Text>
+        <Image source={wateringImg} style={styles.image}/>
+        <Text style={styles.subtitle}>
+            Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
+        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+            <Text style={styles.buttonText}>
+                >
+            </Text>
+        </TouchableOpacity>
+    </SafeAreaView>
+  )
+}
+
+const styles =StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems:'center',
+        justifyContent: 'space-between'        
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading
+    },
+    subtitle : {
+        fontSize: 18,
+        textAlign: 'center',
+        paddingHorizontal:20,
+        color: colors.heading
+    },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius:16,
+        marginBottom:10,
+        height:56,
+        width:56
+    },
+    image: {
+        width:292,
+        height:284
+    },
+    buttonText: {
+        fontSize: 24,
+        color: colors.white
+    }
+
+})
